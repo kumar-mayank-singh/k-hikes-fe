78edd0f885e96d8bf5a38fd249f304072dfdd3a7
kumar-mayank-singh <singh.mayank.github@gmail.com>
2026-07-02 12:41:37 +0530

    added API base url

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,8 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://0.0.0.0:3001/api/:path*",
+        destination:
+          "https://karnataka-hikes-abggffgrdchshrem.southindia-01.azurewebsites.net/api/:path*",
       },
       {
         source: "/uploads/:path*",
