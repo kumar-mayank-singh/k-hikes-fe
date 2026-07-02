3885c914952328e37a9828c6a6b8768992c3debd
kumar-mayank-singh <singh.mayank.github@gmail.com>
2026-07-02 12:03:48 +0530

    updated the packages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   distDir: "build",
+  output: "standalone",
   allowedDevOrigins: ["*"],
+
   async rewrites() {
     return [
       {
